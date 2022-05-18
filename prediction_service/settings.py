d0f0d3122bbe0f688455899d64568a0a8a863cf2
rzepka <nathalie.rzepka@htw-berlin.de>
2022-05-18 10:16:07 +0200

    changed db connection

--- prediction_service/settings.py
+++ prediction_service/settings.py
@@ -89,11 +89,17 @@ WSGI_APPLICATION = 'prediction_service.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'orthogra_db1',
+        # 'USER': 'root',
+        # 'PASSWORD':'test',
+        # 'HOST':'host.docker.internal',#'db',#localhost
+        # 'PORT':'3306'
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'orthogra_db1',
-        'USER': 'root',
-        'PASSWORD':'test',
-        'HOST':'host.docker.internal',#'db',#localhost
+        'USER': 'orthogra_1',
+        'PASSWORD':'mZGK37Gt1cGBmx8F',
+        'HOST':'188.40.29.147', #'host.docker.internal',#'db',#localhost
         'PORT':'3306'
      }
 }
