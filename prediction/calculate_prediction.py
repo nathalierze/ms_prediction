3b82dc2482a08412a65f16a2f5cf62996e86f75e
rzepka <nathalie.rzepka@htw-berlin.de>
2022-06-02 14:49:16 +0200

    fixed jahre dabei calculation

--- prediction/calculate_prediction.py
+++ prediction/calculate_prediction.py
@@ -230,7 +230,12 @@ def get_sex(sex):
 def get_jahre_dabei(userID):
     user = schueler.objects.get(pk=userID)
     serializer = SchuelerSerializer(user)
-    jahre_dabei = int(serializer.data['Klassenstufe']) - int(serializer.data['Anmeldeklassenstufe'])
+
+    klassenstufen = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13']
+    jahre_dabei = 0
+    if(serializer.data['Klassenstufe'] in klassenstufen ){
+        jahre_dabei = int(serializer.data['Klassenstufe']) - int(serializer.data['Anmeldeklassenstufe'])
+    }
 
     return jahre_dabei
 
