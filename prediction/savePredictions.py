56bbe2c6f0000b12e976da30e240ee47295c784e
rzepka <nathalie.rzepka@htw-berlin.de>
2022-06-02 18:36:27 +0200

    changed timezone

--- prediction/savePredictions.py
+++ prediction/savePredictions.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+import pytz
+from pytz import timezone
 from prediction.serializers import PredictionsSerializer
 from .models import predictions, schueler, saetze
 
 def sendReport(data, rounded_pred):
+    german_tz = pytz.timezone('Europe/Berlin')
     user = schueler.objects.get(pk=data['UserID'])
     satz = saetze.objects.get(pk=data['satzID'])
-    report = predictions(UebungsID=data['UebungsID'], SatzID=data['satzID'], UserID=data['UserID'], interventiongroup=user.interventiongroup, prediction=rounded_pred, seqMode=data['seqMode'], modus=data['Testposition'], versionline=data['versionline'], version=satz.Versionsnr, Datum=datetime.now())
+    report = predictions(UebungsID=data['UebungsID'], SatzID=data['satzID'], UserID=data['UserID'], interventiongroup=user.interventiongroup, prediction=rounded_pred, seqMode=data['seqMode'], modus=data['Testposition'], versionline=data['versionline'], version=satz.Versionsnr, Datum=datetime.now(tz=german_tz))
     report.save()
