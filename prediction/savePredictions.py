1427011fef230d60539901f843d20ba4e7299699
rzepka <nathalie.rzepka@htw-berlin.de>
2022-06-02 18:45:17 +0200

    print tryout

--- prediction/savePredictions.py
+++ prediction/savePredictions.py
@@ -6,6 +6,7 @@ from .models import predictions, schueler, saetze
 
 def sendReport(data, rounded_pred):
     german_tz = pytz.timezone('Europe/Berlin')
+    print(datetime.now(tz=german_tz))
     user = schueler.objects.get(pk=data['UserID'])
     satz = saetze.objects.get(pk=data['satzID'])
     report = predictions(UebungsID=data['UebungsID'], SatzID=data['satzID'], UserID=data['UserID'], interventiongroup=user.interventiongroup, prediction=rounded_pred, seqMode=data['seqMode'], modus=data['Testposition'], versionline=data['versionline'], version=satz.Versionsnr, Datum=datetime.now(tz=german_tz))
